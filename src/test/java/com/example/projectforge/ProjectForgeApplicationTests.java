a65787015b7cd7306d2263979c6e6b09ce7041b5
Asger <asgervb@gmail.com>
2024-04-27 22:57:41 +0200

    Initial commit

---
+++ src/test/java/com/example/projectforge/ProjectForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
