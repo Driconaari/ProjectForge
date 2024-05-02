8d3583d67426dc2a6014049f13dfa475b9433de8
Asger <asgervb@gmail.com>
2024-05-02 16:11:19 +0200

    added @GeneratedValue to the User class

--- src/main/java/com/example/projectforge/model/User.java
+++ src/main/java/com/example/projectforge/model/User.java
@@ -2,11 +2,14 @@ package com.example.projectforge.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String email;
