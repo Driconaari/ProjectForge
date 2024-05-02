1da29695f6c6fae64632933ff0b7a59c4e03bc2d
Asger <asgervb@gmail.com>
2024-05-02 10:55:22 +0200

    added the register method and allowed peopl to register

--- src/main/java/com/example/projectforge/security/SecurityConfig.java
+++ src/main/java/com/example/projectforge/security/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
      http
                  .authorizeRequests((requests) -> requests
-                            .requestMatchers("/login").permitAll()
+                            .requestMatchers("/login","register").permitAll()
                             .anyRequest().authenticated()
                  )
                  .formLogin((formLogin) -> formLogin
