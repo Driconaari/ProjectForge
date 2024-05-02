d08499e1f0e455378931064487fdfb98d4dfed5d
Asger <asgervb@gmail.com>
2024-05-02 16:13:40 +0200

    Gave a @Table users to the repo

--- src/main/java/com/example/projectforge/model/User.java
+++ src/main/java/com/example/projectforge/model/User.java
@@ -1,12 +1,10 @@
 package com.example.projectforge.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
