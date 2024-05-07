302ef2c9110296abdd172329daac68a7d02c6eff
Asger <asgervb@gmail.com>
2024-05-07 18:19:00 +0200

    added to the component scan

--- src/main/java/com/example/projectforge/ResourceRepository/ResourceAllocationRepository.java
+++ src/main/java/com/example/projectforge/ResourceRepository/ResourceAllocationRepository.java
@@ -1,4 +1,9 @@
 package com.example.projectforge.ResourceRepository;
 
-public class ResourceAllocationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.projectforge.model.ResourceAllocation;
+
+@Repository
+public interface ResourceAllocationRepository extends JpaRepository<ResourceAllocation, Long> {
 }
