81b287a9e5ec5e3d52bed30838a62efc5b5da335
Asger <asgervb@gmail.com>
2024-05-17 11:39:13 +0200

    changed insert.sql

--- src/main/resources/insert.sql
+++ src/main/resources/insert.sql
@@ -5,10 +5,9 @@ INSERT INTO role (role) VALUES ('USER 1');
 INSERT INTO role (role) VALUES ('ADMIN 2');
 
 -- Insert into user
-INSERT INTO user (username, password, role_id) VALUES ('bob', 'bob', 1);
-INSERT INTO user (username, password, role_id) VALUES ('User2', 'Password2', 2);
-INSERT INTO user (username, password, role_id) VALUES ('1', '1', 2);
-
+INSERT INTO user (username, password, email, role_id) VALUES ('Alice', 'password1', 'alice@example.com', 1);
+INSERT INTO user (username, password, email, role_id) VALUES ('bob', 'bob', 'bob@example.com', 2);
+INSERT INTO user (username, password, email, role_id) VALUES ('Charlie', 'password3', 'charlie@example.com', 2);
 -- Insert into project
 INSERT INTO project (project_name, project_description, start_date, end_date, user_id) VALUES ('Project 1', 'Description 1', '2022-01-01', '2022-12-31', 1);
 INSERT INTO project (project_name, project_description, start_date, end_date, user_id) VALUES ('Project 2', 'Description 2', '2023-01-01', '2023-12-31', 2);
