3d0f560f7433e5d112254b55a65358785e3e40c8
Asger <asgervb@gmail.com>
2024-05-26 16:56:08 +0200

    made UserRepositoryTest

---
+++ src/test/java/com/example/projectforge/repository/UserRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.example.projectforge.repository;
+
+import com.example.ProjectForge.model.User;
+import com.example.ProjectForge.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserRepositoryTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    public void testRegisterUser() {
+        //Test data
+        User user = new User(1, "TestUser", "password", "testuser@example.com");
+
+        //Register user method
+        userRepository.register(user);
+
+        //Verify the user is registered
+        verify(userRepository, times(1)).register(user);
+    }
+
+    @Test
+    public void testEditUser() {
+        //Test data
+        User user = new User(1, "TestUser", "password", "testuser@example.com");
+
+        //Edit user method
+        userRepository.editUser(user, user.getUser_id());
+
+        //Verify user is updated
+        verify(userRepository, times(1)).editUser(user, user.getUser_id());
+    }
+
+    @Test
+    public void testIsUsernameTaken() {
+        //Test data
+        String username = "TestUser";
+
+        //Check username method
+        userRepository.isUsernameTaken(username);
+
+        //Verify method is called
+        verify(userRepository, times(1)).isUsernameTaken(username);
+    }
+}
