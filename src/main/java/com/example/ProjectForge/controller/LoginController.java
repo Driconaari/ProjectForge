df2662bc110698de3248922cfa7a3ef6e06a00ec
Asger <asgervb@gmail.com>
2024-05-27 09:21:42 +0200

    LoginController

--- src/main/java/com/example/ProjectForge/controller/LoginController.java
+++ src/main/java/com/example/ProjectForge/controller/LoginController.java
@@ -12,56 +12,42 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
-//Login controller with login, register, logout, edit user, home and session timeout pages
-
-//Path for login controller is /login and all pages are under /login path with different paths for each page in controller class
-// below with @RequestMapping annotation for each page path in controller class and @GetMapping and @PostMapping annotations for each
-@RequestMapping(path = "")
 @Controller
+@RequestMapping("/")
 public class LoginController {
 
-    private UserService userService;
-    private RoleService roleService;
+    private final UserService userService;
+    private final RoleService roleService;
 
     public LoginController(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
     }
-//Check if user is signed in with session and return true or false if user is signed in or not,
-// signed in with session attribute user from session object
-    public boolean isLoggedIn(HttpSession session) {
+
+    private boolean isLoggedIn(HttpSession session) {
         return session.getAttribute("user") != null;
     }
 
-    //Index page shows login in or register page for user
-    @GetMapping(path = "/")
+    @GetMapping
     public String index() {
         return "User/index";
     }
 
-    //Page users see when session times out and they need to login again
-    @GetMapping(path = "/sessionTimeout")
+    @GetMapping("/sessionTimeout")
     public String sessionTimeout() {
         return "User/sessionTimeout";
     }
 
-
-    //Page users see when they login with user_id
-    @GetMapping(path = "/home/{user_id}")
-    public String homeOrg(Model model, @PathVariable int user_id) {
-        model.addAttribute("user_id", user_id);
+    @GetMapping("/home/{user_id}")
+    public String homeOrg(Model model, @PathVariable("user_id") int userId) {
+        model.addAttribute("user_id", userId);
         return "User/home";
     }
 
-
-    //Checks if user is in current session and redirects to login page if not connected
-    @GetMapping(path = "/login")
+    @GetMapping("/login")
     public String isUserConnected(HttpSession session, Model model) {
-        //Organization object from session
         User user = (User) session.getAttribute("user");
 
-        //If not connected redirect to login page. if connected continue to home page
         if (user == null) {
             model.addAttribute("user", new User());
             return "User/login";
@@ -70,92 +56,75 @@ public class LoginController {
         }
     }
 
+    @PostMapping("/login")
+    public String login(HttpSession session, @ModelAttribute("user") User user, Model model) {
+        try {
+            User userLogin = userService.login(user.getUsername(), user.getPassword());
+            if (userLogin != null) {
+                session.setAttribute("user", userLogin);
+                session.setMaxInactiveInterval(1500);
 
-    //Sign in with user and redirect to home page with user_id
-   @PostMapping(path = "/login")
-public String login(HttpSession session, @ModelAttribute("user") User user, Model model) {
-    try {
-        User userLogin = userService.login(user.getUsername(), user.getPassword());
-        if (userLogin != null) {
-            session.setAttribute("user", userLogin);
-            session.setMaxInactiveInterval(1500);
+                if (userLogin.getRole_id() != 0) {
+                    session.setAttribute("role", userLogin.getRole_id());
+                }
 
-            // If the user has a role, set it in the session
-            if (userLogin.getRole_id() != 0) {
-                session.setAttribute("role", userLogin.getRole_id());
+                return "redirect:/home/" + userLogin.getUser_id();
+            } else {
+                model.addAttribute("loginFailed", true);
+                return "User/login";
             }
-
-            return "redirect:/home/" + userLogin.getUser_id();
-        } else {
-            model.addAttribute("loginFailed", true);
-            return "User/login";
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-    } catch (Exception e) {
-        throw new RuntimeException(e);
     }
-}
 
-
-    //Sign up page with user object and role_id from database to sign up user
-    @GetMapping(path = "/register")
+    @GetMapping("/register")
     public String showSignUp(Model model) {
         List<Role> roles = roleService.getAllRoles();
         User user = new User();
 
         model.addAttribute("user", user);
-        model.addAttribute("defualtRole", 1);//Default role is 1 for user role in database table role (1 = user, 2 = admin)
+        model.addAttribute("defaultRole", 1);
         return "User/register";
     }
 
-    //Sign up with user and redirect to login page
-   @PostMapping(path = "/register")
-public String register(@ModelAttribute("user") User user, @RequestParam("role_id") int role_id, @RequestParam("email") String email, Model model) {
-    boolean isUsernameTaken = userService.isUsernameTaken(user.getUsername());
+    @PostMapping("/register")
+    public String register(@ModelAttribute("user") User user, @RequestParam("role_id") int roleId, @RequestParam("email") String email, Model model) {
+        if (userService.isUsernameTaken(user.getUsername())) {
+            model.addAttribute("usernameTaken", true);
+            return "User/register";
+        }
 
-    user.setRole_id(role_id);
-
-    if (isUsernameTaken) {
-        model.addAttribute("usernameTaken", true);
-        return "User/register";
+        user.setRole_id(roleId);
+        user.setEmail(email);
+        userService.register(user);
+        return "redirect:/login";
     }
 
-    user.setRole_id(role_id);
-    user.setEmail(email); // Set the email field on the user
-    userService.register(user);
-    return "redirect:/login";
-}
-
-
-    //Sign out and redirect to login page
-    @GetMapping(path = "/logout")
+    @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
         return "redirect:/register";
     }
 
-
-    //Edit user page with user_id and user object from database to edit user information in database
-    @GetMapping(path = "/editUser/{user_id}")
-    public String showEditUser(@PathVariable("user_id") int user_id, Model model, HttpSession session) {
-
+    @GetMapping("/editUser/{user_id}")
+    public String showEditUser(@PathVariable("user_id") int userId, Model model, HttpSession session) {
         if (isLoggedIn(session)) {
-            model.addAttribute("user_id", user_id);
-            User user = userService.getUserFromId(user_id);
+            User user = userService.getUserFromId(userId);
             model.addAttribute("user", user);
             return "User/editUser";
         }
         return "redirect:/sessionTimeout";
     }
 
-    //Edit user information in database and redirect to home page with user_id
-    @PostMapping(path = "/editUser/{user_id}")
-    public String editUser(@PathVariable("user_id") int user_id, @RequestParam("username") String user_name, String password, HttpSession session) {
+    @PostMapping("/editUser/{user_id}")
+    public String editUser(@PathVariable("user_id") int userId, @RequestParam("username") String username, @RequestParam("password") String password, HttpSession session) {
         if (isLoggedIn(session)) {
-            User user = userService.getUserFromId(user_id);
-            user.setUsername(user_name);
+            User user = userService.getUserFromId(userId);
+            user.setUsername(username);
             user.setPassword(password);
-            userService.editUser(user, user_id);
-            return "redirect:/home/" + user_id;
+            userService.editUser(user, userId);
+            return "redirect:/home/" + userId;
         }
         return "redirect:/sessionTimeout";
     }
